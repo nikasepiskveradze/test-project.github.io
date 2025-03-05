a9eeb2494491ff2d5ea0d5e076c4dc953c4d1233
Nika Sepiskveradze <n.sepiskveradze@mymarket.ge>
2025-03-05 21:57:22 +0400

    initial

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+
+    fetch('https://api.mymarket.ge/api/ka/products', {method:'POST'}).then(response => response.json()).then(data => {
+      console.log('data', data?.data?.Prs)
+      setProducts(data?.data?.Prs)
+    })
+
+  }, []) 
+
+  console.log('products',products);
+  
+
+  return (
+    <>
+      <div>nika</div>
+    </>
+  )
+}
+
+export default App
